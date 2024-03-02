fb74866e9b2675c479ed564317d5e162c7423165
Mohammed Jabri <160414351+Romiche33@users.noreply.github.com>
2024-03-02 13:05:04 +0100

    first commit

---
+++ administrator/logs/error.php
@@ -0,0 +1,10 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2024-02-25 19:17:17 UTC
+#Software: Joomla! 5.0.3 Stable [ Kuboresha ] 20-February-2024 16:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2024-02-25T19:17:17+00:00	INFO ::1	joomlafailure	Le mot de passe ne correspond pas au nom d'utilisateur, ou vous n'avez pas encore de compte.
+2024-02-25T19:17:34+00:00	INFO ::1	joomlafailure	Le mot de passe ne correspond pas au nom d'utilisateur, ou vous n'avez pas encore de compte.
+2024-02-25T19:48:35+00:00	INFO ::1	joomlafailure	Le mot de passe ne correspond pas au nom d'utilisateur, ou vous n'avez pas encore de compte.
+2024-02-25T19:48:50+00:00	INFO ::1	joomlafailure	Le mot de passe ne correspond pas au nom d'utilisateur, ou vous n'avez pas encore de compte.
---
+++ administrator/logs/joomla_scheduler.php
@@ -0,0 +1,11 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2024-02-28 08:27:07 UTC
+#Software: Joomla! 5.0.3 Stable [ Kuboresha ] 20-February-2024 16:00 GMT
+
+#Fields: date	time	priority	message
+2024-02-28	08:27:07	INFO	Tâche #02 'Session GC' en cours d'exécution.
+2024-02-28	08:27:07	INFO	Tâche> SessionGC end
+2024-02-28	08:27:08	INFO	La tâche #02 s'est terminée avec succès en 0.00 (net 0.04) secondes.
+2024-02-28	08:38:19	INFO	Tâche #03 'Update Notification' en cours d'exécution.
+2024-02-28	08:38:22	INFO	La tâche #03 s'est terminée avec succès en 3.03 (net 3.03) secondes.
